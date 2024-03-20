f5fdcdee438cd308c998cf49d60d60ba84fd116a
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 12:16:49 -0700

    Unique addition

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    # Use the symmetric difference operator '^' to find elements
+    # that are in either set_1 or set_2 but not in both.
+    return set_1 ^ set_2
