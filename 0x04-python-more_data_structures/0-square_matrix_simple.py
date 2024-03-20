548deac315126c251bed81519daff405fa9adece
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 21:42:47 +0300

    Search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Use a list comprehension to iterate through rows and then each element in the row
-    # Square each element and construct the new row, and then construct the new matrix
-    return [list(map(lambda x: x * x, row))for row in matrix]
+    new_matrix = []  # Create a new matrix to store the squared values
+    for row in matrix: # Iterate through the rows of the matrix
+        new_matrix.append(list(map(lambda x: x**2, row))) # Square each element in the row and append to new_matrix
+    return new_matrix # Return the new matrix
