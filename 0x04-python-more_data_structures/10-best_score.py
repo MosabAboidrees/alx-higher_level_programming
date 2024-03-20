0abf43893e776b09eb9fda5ab9a4d19e2a2bda1c
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 12:58:23 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    # Check if the dictionary is None.
     if a_dictionary is None:
         return None
-    best_value = 0 # Assume the first key has the largest value.
-    best_key = None # The key associated with the largest value.
+    # Assume the first key has the largest value.
+    best_value = 0
+    # The key associated with the largest value.
+    best_key = None
     # Iterate over the keys in the dictionary.
-    for key, value  in a_dictionary.items():
-        # If the current key's value is greater than the largest known value,
+    for key, value in a_dictionary.items():
+        # If the current key's value is
+        # greater than the largest known value,
         # update big_value and big_key to these new maximums.
         if value > best_value:
             best_value = value
