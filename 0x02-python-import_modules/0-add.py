ff9576083874469c206ce4f1588d3df561510124
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 14:13:18 -0700

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,6 @@
 from add_0 import add
 
 if __name__ == "__main__":
-a = 1  # Assigning 1 to variable a
-b = 2  # Assigning 2 to variable b
-# Printing the result of the addition in the required format
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1  # Assigning 1 to variable a
+    b = 2  # Assigning 2 to variable b
+    print("{} + {} = {}".format(a, b, add(a, b)))
