296fffd8b67a0e05de9e57bb32409398e46333af
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 12:05:59 -0700

    Fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # return a new matrix with the square of each element
-    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
+    # Use a list comprehension to iterate through rows and then each element in the row
+    # Square each element and construct the new row, and then construct the new matrix
+    return [[x**2 for x in row] for row in matrix]
