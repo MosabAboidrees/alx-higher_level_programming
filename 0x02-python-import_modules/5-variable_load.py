c69ce894b30dd1dfe07502f10ccc90f6ffaa4f05
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 14:32:33 -0700

    Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from variable_load_5 import a  # Import variable 'a' from 'variable_load_5.py'
-
-    print(a)  # Print the value of 'a'
+    from variable_load_5 import a
+    print(a)
