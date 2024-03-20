f6a7dc989643cbb61cb34d20b3a8ddc084f6e587
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 12:34:59 -0700

    Simple delete by key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,6 +4,7 @@ def simple_delete(a_dictionary, key=""):
     # The pop method removes the item with the provided key and returns its value.
     # If the key doesn't exist, it returns the default value, here None,
     # and does not raise a KeyError.
-    a_dictionary.pop(key, None)
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
     # Return the (potentially) updated dictionary.
     return a_dictionary
