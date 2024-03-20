c1fc6e73fc3f2744851f3148c2f64252238fc1d1
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 12:07:25 -0700

    Fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    # Use a list comprehension to iterate through rows and then each element in the row
-    # Square each element and construct the new row, and then construct the new matrix
-    return [[x**2 for x in row] for row in matrix]
+def search_replace(my_list, search, replace):
+	def find_search(element): # helper function to find and replace
+		# if element is not search, return element, else return replace
+		return element if element != search else replace
+	# return list of elements after find_search is applied to each element
+	return list(map(find_search, my_list))
