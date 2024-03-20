07f2c557adf0b85966e13b3208a6c55b14909403
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 13:15:04 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     # Check if the input is not a string or if it's None, then return 0.
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not isinstance(roman_string, str):
         return 0
     
     # Dictionary to map Roman numerals to their integer values.
