f5dafe90b84de1d6bd5d35f51bf0b8f92f0efdac
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 12:02:54 -0700

    Fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def find_search(element): # helper function to find and replace
-        # if element is not search, return element, else return replace
-        return element if element != search else replace
-    # return list of elements after find_search is applied to each element
-    return list(map(find_search, my_list))
+    return list(map(lambda e: replace if e == search else e, my_list))
