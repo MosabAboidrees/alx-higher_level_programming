20454724c99b289992ae57894dacc4f7217465fb
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 14:25:11 -0700

    My first toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    a = 10  # Assign the value 10 to variable 'a'
-    b = 5   # Assign the value 5 to variable 'b'
-    # Perform operations using the imported functions and print the results.
-    # The use of print function is minimized as per the requirement.
+    a = 10
+    b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
