1fafa7e05eef81a3b475b9d0f21250fec55862f4
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 12:12:12 -0700

    Fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-	def find_search(element): # helper function to find and replace
-		# if element is not search, return element, else return replace
-		return element if element != search else replace
-	# return list of elements after find_search is applied to each element
-	return list(map(find_search, my_list))
+def square_matrix_simple(matrix=[]):
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
