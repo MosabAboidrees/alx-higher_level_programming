e65d90a3b9b93e356d9c4edaa612bb01eb884092
MosabAboidrees <mosab_mahala@yahoo.com>
2024-03-20 14:39:29 -0700

    my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,15 +4,13 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     # Check if the number of arguments is exactly 3 (script name + 3 arguments = 4 total)
-    if len(sys.argv) - 1 != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
+        exit(1)
     # Assign command line arguments to variables
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-
     # Determine the operation based on the operator argument and execute it
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
@@ -25,4 +23,4 @@ if __name__ == "__main__":
     else:
         # If the operator is not recognized, print an error message and exit
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
