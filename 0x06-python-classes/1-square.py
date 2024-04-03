b3d55f9efc33864fbc9a4efa57f6ffe2ae9ed25f
MosabAboidrees <mosab_mahala@yahoo.com>
2024-04-03 13:58:39 -0700

    first square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square by its size"""
+
+    def __init__(self, size):
+        """Initializes the square with a size.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
