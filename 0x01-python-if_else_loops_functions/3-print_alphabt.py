48053d9a31cc67e78cab50594b8c024e66fa8218
MosabAboidrees <mosab_mahala@yahoo.com>
2024-05-09 20:30:22 +0000

    prints the ASCII alphabet fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for char in range(97, 123):  # ASCII values for 'a' to 'z'
     if chr(char) != 'q' and chr(char) != 'e':
+          continue
         print("{}".format(chr(char)), end='')
