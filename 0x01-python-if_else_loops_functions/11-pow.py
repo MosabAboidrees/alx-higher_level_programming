830212b38a03a0cbe5c1cb683598c08b977a5b56
MosabAboidrees <mosab_mahala@yahoo.com>
2024-05-10 11:57:21 +0000

    Computes the power

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the result.
+    :param a: Base integer or float
+    :param b: Exponent integer
+    :return: a raised to the power of b
+    """
+    return a ** b
