4922897b7dc86c95f0355f28f8f9b8b5bb725a7b
MosabAboidrees <mosab_mahala@yahoo.com>
2024-05-10 11:32:05 +0000

    prints the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+def print_last_digit(number):
+    """
+    Prints the last digit of a number and returns it.
+    :param number: Integer whose last digit is to be printed
+    :return: The last digit of the number
+    """
+    last_digit = abs(number) % 10  # Use abs to handle negative numbers correctly
+    print(last_digit, end='')  # Print the last digit, stay on the same line
+    return last_digit
