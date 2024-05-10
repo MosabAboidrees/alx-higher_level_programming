ef4af63e8ce3173beeb02e133a267d05fe54a09f
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-05-10 15:37:24 +0300

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,19 +2,18 @@
 def fizzbuzz():
     """
     Prints numbers from 1 to 100.
-    For multiples of three, prints 'Fizz' instead of the number.
+    For multiples of three, print 'Fizz' instead of the number.
     For multiples of five, prints 'Buzz'.
     For numbers multiples of three and five, print 'FizzBuzz.'
     Each element is followed by a space except for the last element.
     """
-    result = []  # Using a list to store results for precise space control
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
+            print("FizzBuzz", end=' ')
         elif i % 3 == 0:
-            result.append("Fizz")
+            print("Fizz", end=' ')
         elif i % 5 == 0:
-            result.append("Buzz")
+            print("Buzz", end=' ')
         else:
-            result.append(str(i))
-    print(" ".join(result))  # Join all elements with a space and print once
+            print(i, end=' ')
+    print()
