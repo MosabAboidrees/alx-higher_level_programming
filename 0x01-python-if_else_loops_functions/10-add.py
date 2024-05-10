f295697b4b5b68298a31874deffbd2ede2d05c98
MosabAboidrees <mosab_mahala@yahoo.com>
2024-05-10 11:43:10 +0000

    adds two integers and returns the result

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+    :param a: First integer
+    :param b: Second integer
+    :return: Sum of a and b
+    """
+    return a + b
