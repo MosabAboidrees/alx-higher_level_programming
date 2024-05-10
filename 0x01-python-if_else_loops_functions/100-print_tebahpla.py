a460d3fc8336e4cfe0e99cc7a6bea38a57b220e2
MosabAboidrees <mosab_mahala@yahoo.com>
2024-05-10 13:05:29 +0000

    prints the ASCII alphabet, in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for i in range(26):
     # Calculate the ASCII value for the letter to print
     # Start from 'z' (122) and 'Y' (89), decrement by 2
     # for lowercase and by 2 for uppercase alternatively
-    print("{:c}".format(122 - i if i % 2 == 0 else 89 - i), end='')
+    print("{:c}".format(i if i % 2 == 0 else i - 32), end='')
