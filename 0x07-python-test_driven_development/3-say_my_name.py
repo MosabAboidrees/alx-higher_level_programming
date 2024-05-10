d365e1ffe4bd0d97e3a895e49f1d64477e5d53d6
MosabAboidrees <mosab_mahala@yahoo.com>
2024-05-10 18:33:00 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,14 +3,13 @@
 This module defines a function say_my_name which prints the full name.
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Prints the full name from the given first name and last name.
-
     Args:
         first_name (str): The first name to print.
         last_name (str): The last name to print, optional.
-
     Raises:
         TypeError: If either first_name or last_name are not strings.
     """
@@ -20,4 +19,3 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     print("My name is {:s} {:s}".format(first_name, last_name))
-
