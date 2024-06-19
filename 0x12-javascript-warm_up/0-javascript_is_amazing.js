2781db0eca528bbcf4a393f9c6419c43294a7270
MosabAboidrees <mosab_mahala@yahoo.com>
2024-06-19 22:08:18 +0300

    fix

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// Declare a constant variable called myVar and
-// assign it the value "JavaScript is amazing"
+
 const myVar = 'JavaScript is amazing';
-console.log(myVar); // Print the value of myVar to the console
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Print the lines below to the console
+/* Print the lines below to the console */
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
