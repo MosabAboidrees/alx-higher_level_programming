304e3d81ef2562f2c1d5c7c0eee986e869821e7b
MosabAboidrees <mosab_mahala@yahoo.com>
2024-06-19 13:09:55 -0700

    Object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Define an object with type and value properties
+const myObject = {
+  type: 'object',
+  value: 12
+};
+// Print the object before modification
+console.log(myObject);
+// Update the value property of the object to 89
+myObject.value = 89;
+// Print the object after modification
+console.log(myObject);
