26998e141603bf94feb1486eff3f42b7dc5730bb
MosabAboidrees <mosab_mahala@yahoo.com>
2024-06-19 13:12:05 -0700

    Add file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,7 @@
+// Define a function that returns the addition of two integers
+function add (a, b) {
+  return a + b;
+}
+
+// Export the add function to make it visible from outside
+module.exports.add = add;
