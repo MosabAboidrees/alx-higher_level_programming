352244c90ed22d36e9411c7b8b222af344e4efb2
MosabAboidrees <mosab_mahala@yahoo.com>
2024-06-19 13:38:08 -0700

    executes x times a function

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 // Define a function that executes another function x times
-function callMeMoby(x, theFunction) {
-	// Loop x times
-	for (let i = 0; i < x; i++) {
-	  // Execute the provided function
-	  theFunction();
-	}
+function callMeMoby (x, theFunction) {
+  // Loop x times
+  for (let i = 0; i < x; i++) {
+    // Execute the provided function
+    theFunction();
   }
-
-  // Export the callMeMoby function to make it visible from outside
-  module.exports.callMeMoby = callMeMoby;
+}
+// Export the callMeMoby function to make it visible from outside
+module.exports.callMeMoby = callMeMoby;
