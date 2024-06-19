8b00ff803ede570d7206e3f91b42e1062bdb68b1
MosabAboidrees <mosab_mahala@yahoo.com>
2024-06-19 12:48:37 -0700

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Create an array containing the three lines to print
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through each element in the messages array
+for (let i = 0; i < messages.length; i++) {
+  // Print the current element of the messages array
+  console.log(messages[i]);
+}
