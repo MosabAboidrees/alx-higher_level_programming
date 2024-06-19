de6b9e6d8e1c067d592bb904bb1a3ed79a767c26
MosabAboidrees <mosab_mahala@yahoo.com>
2024-06-19 13:55:54 -0700

    Write a class Rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
-// Define a class Rectangle
+/**
+ * Define a class Rectangle
+ */
+
 class Rectangle {
   // Constructor that takes 2 arguments: w and h
-  constructor(w, h) {
+  constructor (w, h) {
     // Initialize the instance attribute width with the value of w
     this.width = w;
     // Initialize the instance attribute height with the value of h
     this.height = h;
   }
 }
-
 // Export the Rectangle class to make it visible from outside
 module.exports = Rectangle;
