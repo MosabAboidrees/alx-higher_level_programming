99c8b8ad38ea202c6f14bb59bccc76c907263357
MosabAboidrees <mosab_mahala@yahoo.com>
2024-06-19 13:25:53 -0700

    Add file

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,21 +1,6 @@
 #!/usr/bin/node
-
-// Define a function that returns the addition of two integers
 function add (a, b) {
   return a + b;
 }
 
-// Export the add function to make it visible from outside
 module.exports = { add };
-
-// Get the arguments passed to the script, including the default ones
-const args = process.argv.slice(2);
-
-// Convert the first argument to an integer
-const a = parseInt(args[0], 10);
-
-// Convert the second argument to an integer
-const b = parseInt(args[1], 10);
-
-// Print the result of the addition
-console.log(add(a, b));
