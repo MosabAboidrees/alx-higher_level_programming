c109bddf2ae84bed2fee1e74045356743cb6b491
MosabAboidrees <mosab_mahala@yahoo.com>
2024-06-19 21:50:32 +0300

    First constant, first print fix

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-// This is the shebang line to specify the interpreter for the script
 
-// Declare a constant variable called myVar and assign it the value "JavaScript is amazing"
+// Declare a constant variable called myVar and
+// assign it the value "JavaScript is amazing"
 const myVar = 'JavaScript is amazing';
-
-// Print the value of myVar to the console
-console.log(myVar);
+console.log(myVar); // Print the value of myVar to the console
