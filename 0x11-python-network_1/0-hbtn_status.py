ec7c906264910a6fa6f08196dd14c03c744c0e7f
MosabAboidrees <mosab_mahala@yahoo.com>
2024-07-10 00:22:52 +0300

    What is my status? #0 fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,13 +7,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read()
-
-            print("\t- type:", type(body))
-            print("\t- content:", body)
-            print("\t- utf8 content:", body.decode('utf-8'))
-    except Exception as e:
-        print("Error:", e)
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
