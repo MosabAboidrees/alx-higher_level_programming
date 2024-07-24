3cde26fda18974e9309a90560cceb2049a3d31ac
MosabAboidrees <mosab_mahala@yahoo.com>
2024-07-24 07:25:23 -0700

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     # Get the URL and email from the command-line arguments
     url = sys.argv[1]
     email = sys.argv[2]
-    
+
     # Create the payload with the email
     payload = {'email': email}
-    
+
     # Send a POST request to the URL with the payload
     response = requests.post(url, data=payload)
 
