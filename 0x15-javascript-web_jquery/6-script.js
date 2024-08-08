9f1ae5fcb120a0d30ac2130f9951785910b3d71a
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-08-08 03:36:03 +0300

    Change the text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// Use the jQuery API to handle the click event on the DIV#update_header
+$('DIV#update_header').click(function() {
+  // Update the text of the <header> element to "New Header!!!"
+  $('header').text('New Header!!!');
+});
