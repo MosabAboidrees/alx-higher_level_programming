803822ff0c1f38c72d07c337e282246c978753fa
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-08-08 03:32:27 +0300

    Add `.red` class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+// Use the jQuery API to handle the click event on the DIV#red_header
+$('DIV#red_header').click(function() {
+  // Add the 'red' class to the <header> element
+  $('header').addClass('red');
+});
