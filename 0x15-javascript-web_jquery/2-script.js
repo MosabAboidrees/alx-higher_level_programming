77b4604120d111a8b1fdff4f6ffc5b784f03f521
MOSAB ABOIDREES ALTRAIFI YOUSIF <33727845+MosabAboidrees@users.noreply.github.com>
2024-08-08 03:31:25 +0300

    Click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Use the jQuery API to handle the click event on the DIV#red_header
+$('DIV#red_header').click(function() {
+  // Change the color of the <header> element to red
+  $('header').css('color', '#FF0000');
+});
